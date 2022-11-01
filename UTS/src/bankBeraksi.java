36e44b0f4a91cfa5e9b4240b10b4722bec30497f
ariamanggala <110913340+ariamanggala@users.noreply.github.com>
2022-11-01 13:34:44 +0700

    Add files via upload

---
+++ UTS/src/bankBeraksi.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class bankBeraksi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       bank bca = new bank (100000);
+
+        bca.getSaldo();
+        bca.simpanUang(500000);
+        bca.ambilUang(900000);
+       
+    }
+    
+}
